6fb93e9650698e75e9ec75ad07c936c0cfbfc173
germanlizondo <german9lp@gmail.com>
2018-06-29 13:34:01 +0200

    version 3

--- src/app/components/blog.vue
+++ src/app/components/blog.vue
@@ -1,11 +1,89 @@
 <template>
    <div class="page">
-        <div class="blog1">
-          <div class="container text-center">
-           
-        <h1 class="display-4">Benvingut/a al Blog</h1>
-
         
+        <div class="container">
+              <div class="ip3">
+          <div class="container text-left">
+            
+                
+        <h4 class="display-4">Blog</h4>
+        <div class="row">
+             <div class="col-lg-4 col-md-6 col-sm-12  col-xl-4 ">
+       <div class="card">
+          
+  <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Lorem ipsum
+       dolor sit amet consectetur adipisicing elit. Nesciunt, 
+       ducimus sint! Voluptas unde a reiciendis tempore blanditiis
+        quos dolores, adipisci eum rerum? Nam aut minima eligendi quidem 
+        quos similique perferendis! Some quick example text to build on 
+        the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+</div>
+      <div class="col-lg-4 col-md-6 col-sm-12  col-xl-4">
+       <div class="card">
+          
+  <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-12  col-xl-4">
+       <div class="card" >
+          
+  <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+</div>
+       <div class="col-lg-4 col-md-6 col-sm-12  col-xl-4 ">
+       <div class="card">
+          
+  <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Lorem ipsum
+       dolor sit amet consectetur adipisicing elit. Nesciunt, 
+       ducimus sint! Voluptas unde a reiciendis tempore blanditiis
+        quos dolores, adipisci eum rerum? Nam aut minima eligendi quidem 
+        quos similique perferendis! Some quick example text to build on 
+        the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+</div>
+       <div class="col-lg-4 col-md-6 col-sm-12  col-xl-4 ">
+       <div class="card">
+          
+  <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Lorem ipsum
+       dolor sit amet consectetur adipisicing elit. Nesciunt, 
+       ducimus sint! Voluptas unde a reiciendis tempore blanditiis
+        quos dolores, adipisci eum rerum? Nam aut minima eligendi quidem 
+        quos similique perferendis! Some quick example text to build on 
+        the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+</div>
+
+
+
+
+</div>
+        </div>
         </div>
         </div>
     </div>
